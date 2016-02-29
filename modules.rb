183005a0a047d80add1cfc9697eebb00a1b21472
jnf <jeremy.flores@gmail.com>
2016-02-29 14:10:28 -0800

    example of modules and constant scope.

---
+++ modules.rb
@@ -0,0 +1,37 @@
+SOUND = "ploink!"
+
+module CuteAnimals
+  SOUND = "dawwwwwwwww"
+
+  class Dog
+    SOUND = "Woof!"
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  class Cat
+    SOUND = "Meow!"
+    def initialize(name)
+      @name = name
+    end
+  end
+end
+
+module GrossAnimals
+  SOUND = "ugh!"
+
+  class Human
+    SOUND = "Meh."
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  class Slug
+    SOUND = "fart"
+    def intialize(name)
+      @name = name
+    end
+  end
+end
