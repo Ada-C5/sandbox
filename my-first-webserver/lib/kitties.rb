52397d869ae2d80763ad273f94c804fad299fe91
jnf <jeremy.flores@gmail.com>
2016-04-07 11:53:53 -0700

    our pizza party live code.

---
+++ my-first-webserver/lib/kitties.rb
@@ -0,0 +1,4 @@
+class Kitties
+  KITTIES = %w(apple aubergin avocado brocoli cantelop carrot cashew cucumber garlic kiwi lemon lime papaya peach pickles pineappl pumking rhubarb rutebega thyme tomato watermel zuchinni)
+
+end
--- my-first-webserver/myapp.rb
+++ my-first-webserver/myapp.rb
@@ -1,19 +1,36 @@
 require 'sinatra'
 require_relative 'lib/peep'
+require_relative 'lib/kitties'
 
 class MyApp < Sinatra::Base
+  get '/pizza-party' do
+    @cats = Kitties::KITTIES
+    erb :'pizza-party'
+  end
+
+
+
+
+
+
+
+
+
+
+
   get '/my-first-form' do
     erb :my_first_form
   end
 
 
   post '/my-first-form' do
-    @my_peep = Peep.new(params["peep"])
+    # @my_peep = Peep.new(params["peep"])
     erb :my_first_form
   end
 
   get '/kitties' do
-    @cats = %w(apple aubergin avocado brocoli cantelop carrot cashew cucumber garlic kiwi lemon lime papaya peach pickles pineappl pumking rhubarb rutebega thyme tomato watermel zuchinni)
+    @cats = Kitties::KITTIES
+    # @cats = %w(apple aubergin avocado brocoli cantelop carrot cashew cucumber garlic kiwi lemon lime papaya peach pickles pineappl pumking rhubarb rutebega thyme tomato watermel zuchinni)
 
     erb :kitties
   end
